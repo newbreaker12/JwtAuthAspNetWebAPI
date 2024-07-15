4d9532d7b4af2234626c494baea7df2b38bedb3d
newbr <francesco.bigi.87@gmail.com>
2024-07-15 11:05:38 +0200

    Refactor Weather resources

--- JwtAuthAspNetWebAPI/Controllers/WeatherForecastController.cs
+++ JwtAuthAspNetWebAPI/Controllers/WeatherForecastController.cs
@@ -13,21 +13,12 @@ namespace JwtAuthAspNetWebAPI.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+       
+        [HttpGet]
+        [Route("Get")]
+        public IActionResult Get()
         {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok(Summaries);
         }
     }
 }
